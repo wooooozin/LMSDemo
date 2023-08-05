a673583fff8eb71d59ba2df043727e32e6596446
wooooozin <wooooozin@gmail.com>
2023-08-06 08:31:40 +0900

    배너 컨트롤러 의존성 주입 및 http 메서드 추가

--- src/main/java/com/zerobase/fastlms/admin/controller/AdminBannerController.java
+++ src/main/java/com/zerobase/fastlms/admin/controller/AdminBannerController.java
@@ -1,9 +1,15 @@
 package com.zerobase.fastlms.admin.controller;
 
+import com.zerobase.fastlms.admin.dto.BannerDto;
+import com.zerobase.fastlms.admin.model.BannerInput;
 import com.zerobase.fastlms.admin.service.BannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -11,5 +17,25 @@ public class AdminBannerController {
 
     private final BannerService bannerService;
 
+    @PostMapping("/admin/banner/add.do")
+    public String add(Model model, BannerInput parameter) {
 
+        boolean result = bannerService.add(parameter);
+
+        if (result) {
+            return "redirect:/admin/banner/list.do";
+        } else {
+            model.addAttribute("errorMessage", "배너 추가에 실패했습니다.");
+            return "admin/banner/add"; // 실패시 다시 추가 페이지로 이동
+        }
+    }
+
+    @GetMapping("/admin/banner/list.do")
+    public String list(Model model) {
+
+        List<BannerDto> list = bannerService.list();
+        model.addAttribute("list", list);
+
+        return "admin/banner/list";
+    }
 }
