a7af89c1f55acbb993956674f62e5f1611096ccf
wooooozin <wooooozin@gmail.com>
2023-08-06 08:11:30 +0900

    배너 서비스 추가 및 배너컨트롤러 구현

--- src/main/java/com/zerobase/fastlms/admin/service/BannerService.java
+++ src/main/java/com/zerobase/fastlms/admin/service/BannerService.java
@@ -1,19 +1,26 @@
 package com.zerobase.fastlms.admin.service;
 
+import com.zerobase.fastlms.admin.dto.BannerDto;
 import com.zerobase.fastlms.admin.entity.Banner;
 import com.zerobase.fastlms.admin.model.BannerInput;
 import com.zerobase.fastlms.admin.repository.BannerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class BannerService {
 
     private final BannerRepository bannerRepository;
+
+    private Sort getSortBySortOrderDesc() {
+        return Sort.by(Sort.Direction.DESC, "sortOrder");
+    }
 
     public boolean add(BannerInput parameter) {
         MultipartFile imageFile = parameter.getImage();
@@ -40,5 +47,10 @@ public class BannerService {
         bannerRepository.save(banner);
         return true;
 
+    }
+
+    public List<BannerDto> list() {
+        List<Banner> banners = bannerRepository.findAll(getSortBySortOrderDesc());
+        return BannerDto.of(banners);
     }
 }
